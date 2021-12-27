be1a45e48724bbe4f3c96436b8c9f8764f638400
idochetrit <idochetrit.dev@gmail.com>
2021-12-27 14:11:28 +0200

    adds echo

--- bin/release.sh
+++ bin/release.sh
@@ -11,43 +11,44 @@ if [ "$OG_BRANCH" != 'staging' ] || [[ -n $(git status -s) ]] || [[ -n $(git log
 fi
 
 REPO_NAME=$(git remote get-url origin)
-echo "${cyan}cd to temp repo"
+echo "${cyan}cd to temp repo\n"
 git clone $REPO_NAME temp_repo
 cd temp_repo
 
 # ensure we're not double tagging
-echo "${cyan}validate double tag${reset}"
+echo "${cyan}validate double tag${reset}\n"
 CURRENT_TAG=$(git tag --points-at HEAD)
 if [ -n "$CURRENT_TAG" ]; then
   echo "${red}Can't tag this commit again. Commit already tagged with ${CURRENT_TAG}."
   exit 1
 fi
 
-echo "${yellow}pull last changes on master and staging${reset}"
+echo "${yellow}pull last changes on master and staging${reset}\n"
 git fetch --tags
 git checkout master
 git pull --set-upstream --rebase
 git checkout staging
 git pull --set-upstream --rebase
 
-echo "${yellow}tag staging branch${reset}"
+echo "${yellow}tag staging branch${reset}\n"
 LAST_COMMIT_SEMVER=$(git log --format=oneline -n 1 $(git rev-parse HEAD)  | sed -n 's/.*\[\(major|minor|patch\)\].*/\1/p')
 npm version ${LAST_COMMIT_SEMVER:=minor} -m "version %s [skip ci]"
 TAGGED_VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
 
-echo "${yellow}rebase master and merge (ff)${reset}"
+echo "${yellow}rebase master and merge (ff)${reset}\n"
 git rebase staging master
 
-echo "${yellow}push changes${reset}"
+echo "${yellow}push changes${reset}\n"
 git push origin staging
 git push origin master
 git push origin tag v$TAGGED_VERSION
 
 git log --all --color --oneline --decorate --graph -n 20
 
-echo "${cyan}clean temp repo"
+echo "${cyan}clean temp repo\n"
 cd ..
-cd rm -rf ./temp_repo
+echo $(pwd)
+cd rm -rf temp_repo
 git fetch origin master:master
 git fetch origin staging:staging
 
