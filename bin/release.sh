7c42fd0b5f0349d55ac9051f9c25f80fcf7ee445
idochetrit <idochetrit.dev@gmail.com>
2021-12-27 13:38:50 +0200

    coloring

--- bin/release.sh
+++ bin/release.sh
@@ -2,6 +2,7 @@ export yellow="\033[1;33m"
 export red="\033[1;31m"
 export cyan="\033[1;36m"
 export green="\033[1;32m"
+export reset="\033[m"
 
 
 # ensure we're not double tagging
@@ -21,21 +22,21 @@ REPO_NAME=$(git remote get-url origin)
 git clone $REPO_NAME temp_repo
 cd temp_repo
 
-echo "${yellow}pull last changes on master and staging"
+echo "${yellow}pull last changes on master and staging${reset}"
 git checkout master
 git pull --set-upstream --rebase
 git checkout staging
 git pull --set-upstream --rebase
 
-echo "${yellow}tag staging branch"
+echo "${yellow}tag staging branch${reset}"
 LAST_COMMIT_SEMVER=$(git log --format=oneline -n 1 $(git rev-parse HEAD)  | sed -n 's/.*\[\(major|minor|patch\)\].*/\1/p')
 npm version ${LAST_COMMIT_SEMVER:=minor} -m "version %s [skip ci]"
 TAGGED_VERSION=$(awk '/version/{gsub(/("|",)/,"",$2);print $2};' package.json)
 
-echo "${yellow}rebase master and merge (ff)"
+echo "${yellow}rebase master and merge (ff)${reset}"
 git rebase staging master
 
-echo "${yellow}push changes"
+echo "${yellow}push changes${reset}"
 git push origin staging
 git push origin master
 git push origin tag v$TAGGED_VERSION
